b4ba0004c624f1217ec4fa3dd622e096324718bf
tranchikhang <tranchikhang@outlook.com>
2020-08-04 22:53:41 +0900

    login validation

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,55 @@
 var m = require('mithril');
 
+// This is a MithrilJs component
+const UserNameInput = {
+    view() {
+        return [
+            m('input', {
+                className: this.error ? 'error' : '',
+                oninput() {
+                    console.log(this.value)
+                },
+                error: '',
+                placeholder: 'User Name',
+                type: 'text',
+                formcontrolname: 'user',
+                validate() {
+                    console.log(this)
+                    this.error = this.value.length == 0 ? 'Please input user name' : '';
+                }
+            })
+            // m('p.errorMessage', error)
+        ];
+    }
+};
+
+function LoginForm() {
+    return m('form', [
+        m('h1',
+            'Login'
+        ),
+        // passing compnent
+        m(UserNameInput),
+        m('input', {
+            placeholder: 'Password',
+            type: 'password',
+            formcontrolname: 'password'
+        }),
+        m('button', {
+                class: 'pure-button pure-button-primary',
+                id: 'loginBtn',
+                type: 'button',
+                onclick() {
+                    m.route.set('/dashboard')
+                }
+            },
+            'Login'
+        )
+    ])
+}
+
 module.exports = {
-    view: function() {
+    view() {
         return m('div', {
             class: 'pure-g login'
         }, [
@@ -11,31 +59,7 @@ module.exports = {
             m('div', {
                     class: 'pure-u-1-5'
                 },
-                m('form', [
-                    m('h1',
-                        'Login'
-                    ),
-                    m('input', {
-                        placeholder: 'User name',
-                        type: 'text',
-                        formcontrolname: 'user'
-                    }),
-                    m('input', {
-                        placeholder: 'Password',
-                        type: 'password',
-                        formcontrolname: 'password'
-                    }),
-                    m('button', {
-                            class: 'pure-button pure-button-primary',
-                            id: 'loginBtn',
-                            type: 'button',
-                            onclick: function() {
-                                m.route.set('/dashboard')
-                            }
-                        },
-                        'Login'
-                    )
-                ])
+                LoginForm()
             ),
             m('div', {
                 class: 'pure-u-2-5'
